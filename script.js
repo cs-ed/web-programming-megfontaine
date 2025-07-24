8b6f3a679afcebb7bb24c4d4219fae7fc7d4e11b
megfontaine <meggiefon@gmail.com>
2025-07-24 16:26:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function skaBandPlease() {
-  let verbs = ["skankin", "rankin", "vibin", "jumpin", "leapin", "rockin", "sleepin", "boppin", "slippin", "barkin", 
+let name;
+let const verbs = ["skankin", "rankin", "vibin", "jumpin", "leapin", "rockin", "sleepin", "boppin", "slippin", "barkin", 
            "fightin", "yellin", "smellin", "skippin", "screechin"];
-  let nouns = ["pickle", "weasel", "zebras", "muskrats", "walruses", "puppies", "kittens", "lions", "tigers", "parrots",
+let const nouns = ["pickle", "weasel", "zebras", "muskrats", "walruses", "puppies", "kittens", "lions", "tigers", "parrots",
            "giblets", "skunks", "bunnies", "gummies", "dolphins", "grannies", "lads", "lassies", "lizards"];
 
+function skaBandPlease() {
   let randIndex1 = Math.floor(Math.random() * verbs.length);
   let randIndex2 = Math.floor(Math.random() * nouns.length);
-
-  let name = verbs[randIndex1] + nouns[randIndex2];
-
+  name = verbs[randIndex1] + nouns[randIndex2];
   document.getElementById("band_name").innerHTML = name;
 };
 
 window.onload = function() {
   //What should we do when the page first loads?
   skaBandPlease();
-  
-  document.getElementById("I need a 90s SKA Band Name!").addEventListener("click", skaBandPlease);
+};
+
+document.getElementById("I need a 90s SKA Band Name!").addEventListener("click", skaBandPlease);
